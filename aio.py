8da636407425e2b46005218d11404a5d7a2463c7
Jay <jamied_uk@hotmail.com>
2023-04-06 23:56:56 +0100

    Add files via upload

---
+++ aio.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# (c)J~Net 2023
+# jnet.sytes.net
+#
+# ./aio.py
+#
+from Crypto.Cipher import DES3
+from Crypto import Random
+import os
+
+def generate_key():
+    key=Random.get_random_bytes(24)
+    with open("key.txt", "wb") as key_file:
+        key_file.write(key)
+    print("Key generated successfully.")
+
+def get_key():
+    if not os.path.exists("key.txt"):
+        print("Key not found. Please generate a key first.")
+        return
+    with open("key.txt", "rb") as key_file:
+        key=key_file.read()
+    return key
+
+def encrypt_message():
+    key=get_key()
+    if key is None:
+        return
+    message_file=input("Enter the message file to encrypt (default message.txt): ")
+    if message_file == "":
+        message_file="message.txt"
+    if not os.path.exists(message_file):
+        print("Message file not found.")
+        return
+    with open(message_file, "rb") as f:
+        message=f.read()
+    message_length = len(message)
+    padding_length = 8 - (message_length % 8)
+    padding = bytes([padding_length]) * padding_length
+    padded_message = message + padding
+    iv=Random.get_random_bytes(8)
+    cipher=DES3.new(key, DES3.MODE_CBC, iv)
+    ciphertext=cipher.encrypt(padded_message)
+    with open("encrypted.txt", "wb") as f:
+        f.write(iv)
+        f.write(ciphertext)
+    print("Encryption complete.")
+
+
+def decrypt_message():
+    key=get_key()
+    if key is None:
+        return
+    if not os.path.exists("encrypted.txt"):
+        print("Encrypted message not found.")
+        return
+    with open("encrypted.txt", "rb") as f:
+        iv=f.read(8)
+        ciphertext=f.read()
+    cipher=DES3.new(key, DES3.MODE_CBC, iv)
+    plaintext=cipher.decrypt(ciphertext)
+    with open("decrypted.txt", "wb") as f:
+        f.write(plaintext)
+    print("Decryption complete.")
+
+while True:
+    print("==============================")
+    print("TripleDES Encryption Menu")
+    print("==============================")
+    print("1. Generate key")
+    print("2. Encrypt message")
+    print("3. Decrypt message")
+    print("4. Exit")
+    choice=input("Enter your choice (1-4): ")
+    if choice == "1":
+        generate_key()
+    elif choice == "2":
+        encrypt_message()
+    elif choice == "3":
+        decrypt_message()
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice. Please enter a number from 1 to 4.")
+
